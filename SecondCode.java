ff3cd1255c559338f21c99964562f3775edf16a7
sachinsingh1381 <112359892+sachinsingh1381@users.noreply.github.com>
2024-08-03 10:22:05 +0530

    Add files via upload

---
+++ SecondCode.java
@@ -0,0 +1,21 @@
+public class SecondCode {
+    public static void main(String[] args){
+
+        //variables
+
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        //System.out.println(sum);
+
+        int diff = b - a;
+        // System.out.println(diff);
+
+        int mul = a * b;
+        //System.out.println(mul);
+
+        // to calculate equation
+        int ans = (a * b) / (a - b);
+        System.out.println(ans);
+    }
+}
