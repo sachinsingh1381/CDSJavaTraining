5cc432e014f024dcac15f5b9518f9be8bb902d1a
sachinsingh1381 <112359892+sachinsingh1381@users.noreply.github.com>
2024-08-03 10:22:51 +0530

    Add files via upload

---
+++ ThirdClass.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ThirdClass {
+    public static void main(String[] args){
+        //Input
+        Scanner sc = new Scanner(System.in);
+
+        // String name = sc.next();
+       // System.out.println(name);
+
+        String name = sc.nextLine();
+        System.out.println(name);
+
+    }
+}
