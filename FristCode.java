454597d40f7e8367245063615cee4d8bb9111327
sachinsingh1381 <112359892+sachinsingh1381@users.noreply.github.com>
2024-08-03 10:21:49 +0530

    Add files via upload

---
+++ FristCode.java
@@ -0,0 +1,12 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class FristCode {
+    public static void main(String[] args){
+
+        //Output
+
+        System.out.println("HELLO SACHIN");
+        System.out.print("HELLO SACHIN \n");
+        System.out.print("HELLO SACHIN");
+    }
+}
