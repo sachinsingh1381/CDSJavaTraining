33e1984fb925a7b39db3f7e2da0c4e1265f60a31
sachinsingh1381 <112359892+sachinsingh1381@users.noreply.github.com>
2024-08-03 10:22:27 +0530

    Add files via upload

---
+++ StarPattern.java
@@ -0,0 +1,15 @@
+public class StarPattern {
+    public static void main(String[] args){
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+
+
+        System.out.println("\n");
+
+        //to print *
+        //         * *
+        System.out.println("*\n* *");
+    }
+}
